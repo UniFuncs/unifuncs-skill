dfb73596c31ff564d39803cdc2ef9d987214d418
Vinlic科技 <vinlic@vinlic.com>
2026-03-06 12:04:14 +0800

    Update deep-search.sh

--- unifuncs-deep-search/scripts/deep-search.sh
+++ unifuncs-deep-search/scripts/deep-search.sh
@@ -3,7 +3,7 @@
 # 用法: ./deep-search.sh "搜索问题" [model]
 
 QUERY="$1"
-MODEL="${2:-s2}"
+MODEL="${2:-s3}"
 
 if [ -z "$UNIFUNCS_API_KEY" ]; then
     echo "错误: 请设置 UNIFUNCS_API_KEY 环境变量"
@@ -11,7 +11,7 @@ if [ -z "$UNIFUNCS_API_KEY" ]; then
 fi
 
 if [ -z "$QUERY" ]; then
-    echo "用法: ./deep-search.sh \"搜索问题\" [model:s2|s1]"
+    echo "用法: ./deep-search.sh \"搜索问题\" [model:s3|s1]"
     exit 1
 fi
 
